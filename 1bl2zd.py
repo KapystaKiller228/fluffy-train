c2b43ff645a812d5c1bf08e50dc1d0bfc38a2fe1
KapystaKiller228 <70961339+KapystaKiller228@users.noreply.github.com>
2020-09-28 14:33:56 +0700

    Update 1bl2zd.py

--- 1bl2zd.py
+++ 1bl2zd.py
@@ -3,4 +3,5 @@ def is_prime(число):
     for делитель in range(2, int(число**0.5)+1):
         if число % делитель ==0: return False
     return True
+
 print([число for число in range(1, ((int(input('введите верхнюю границу: ')))+1)) if is_prime(число)==True])
