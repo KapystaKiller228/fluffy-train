0c5be279971fe54b14c602a8bb5c55ac0c109e66
KapystaKiller228 <70961339+KapystaKiller228@users.noreply.github.com>
2020-10-03 23:01:01 +0700

    Update 1bl4zd.py

--- 1bl4zd.py
+++ 1bl4zd.py
@@ -12,7 +12,7 @@ def newton(accuracy, x0):
         n=n+1
     return x0
 
-print('This programm solves function by Newton method\nFunction cos(x)-x^3=0 if x =', newton(int(input('Enter accuracy(decimal place): ')), float(input('Enter x0: '))))
-#аналитически это как? просто функцию прописать через через sympy.diff?
+print('This programm solves function by Newton method')
+print('Function cos(x)-x^3=0 if x =', newton(int(input('Enter accuracy(decimal place): ')), float(input('Enter x0: '))))
 #0.865 474 033 102
 #поганые буржуи победили все таки и теперь задачи пишутся для них((((
