cd8beea5a796cb692d5163a84fb97d8b69a7874a
KapystaKiller228 <70961339+KapystaKiller228@users.noreply.github.com>
2020-10-18 20:37:09 +0700

    Update 1bl4zd.py

--- 1bl4zd.py
+++ 1bl4zd.py
@@ -1,23 +1,24 @@
-import math, sympy
+import math, matplotlib.pyplot
 
 def f(x):
-    return sympy.cos(x)-x**3
-
-def derivative():
-    x = sympy.symbols('x')
-    return sympy.diff(sympy.cos(x) - x ** 3, x)
+    return math.cos(x)-x**3
 
 def newton(accuracy, x0):
     eps=10**(accuracy*-1)
     n=1
-    while abs((x0-(f(x0)/derivative()))-x0) > eps:
-        x0=x0-(f(x0)/derivative())
+    array_x=[]
+    array_y=[]
+    while abs((x0-(f(x0)/((f(x0+0.1)-(f(x0)))/0.1)))-x0) > eps:
+        array_x.append(x0)
+        array_y.append(f(x0))
+        x0=x0-(f(x0)/((f(x0+0.1)-(f(x0)))/0.1))
         print('x%s = %s' %(n,x0))
         n=n+1
+        matplotlib.pyplot.plot(array_x,array_y)
+    matplotlib.pyplot.show()
     return x0
 
 print('This programm solves function by Newton method')
-print('Function cos(x)-x^3=0 if x =', newton(int(input('Enter accuracy(decimal place): ')), int(input('Enter x0: '))))
+print('Function cos(x)-x^3=0 if x =', newton(int(input('Enter accuracy(decimal place): ')), float(input('Enter x0: '))))
 #0.865 474 033 102
 #поганые буржуи победили все таки и теперь задачи пишутся для них((((
-#Matplotlib нужон для графика
